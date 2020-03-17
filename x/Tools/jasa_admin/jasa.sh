40846d9b3788458448b90142511624a926d22202
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 23:23:30 +0700

    Create jasa.sh

---
+++ x/Tools/jasa_admin/jasa.sh
@@ -0,0 +1,26 @@
+#usr/bin!/bash
+B='\033[34;1m'
+G='\033[32;1m'
+P='\033[35;1m'
+C='\033[36;1m'
+R='\033[31;1m'
+W='\033[37;1m'
+Y='\033[33;1m'
+BL='\033[0;30m'
+echo "${W}[${Y}0${Y}1${W}]${W} B${W}UA${W}T${W} T${W}OO${W}L${W}S${W} IN${W}T${W}A${W}LL${W}E${W}R"
+z= sleep 1
+echo "${W}[${Y}0${Y}2${W}]${W} M${W}EN${W}C${W}A${W}RI${W} TOO${W}L${W}S${W} T${W}EM${W}B${W}AK${W} P${W}AK${W}ET${Y} X${Y}${Y}L${Y}${Y}"
+z= sleep 1
+echo ${W}"["${Y}"0""${Y}3"${W}"]${W} M${W}ENC${W}A${W}R${W}I${W} T${W}OO${W}L${W}S${W} TEM${W}${W}B${W}A${W}K${W} P${W}AKE${W}T${Y} T${Y}HR${Y}E${Y}E${W}${Y}"
+z= sleep 1
+echo "${W}[${Y}0${Y}4${W}]${W} B${W}UA${W}${W}T${W} TO${W}O${W}${Y}${W}LS${W} P${W}ER${W}${W}C${W}E${W}PA${W}T${Y} In${Y}te${Y}rn${Y}et${Y}"
+z= sleep 1
+echo "${W}[${Y}0${Y}5${W}]${W} C${W}oo${W}m${W}ing${W} So${W}o${W}n.."${W}
+z= sleep 2
+echo ${W}"*Jika Minat Chat Admin Saja!!:)"
+z= sleep 1
+echo ${B}"╔${B}═${B}═${B}═${B}═${B}${B}══${B}═${B}═${B}═${B}══════════════════╗"
+z= sleep 0.2
+echo ${B}"║"${W}"No Admin${R}:""${Y} +6285692258584"
+z= sleep 0.4
+echo ${B}"╚═══════════════════════════╝"${W}
