0b10ba9d9e874797252d5e911e54eccb49e1735d
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 22:22:01 +0700

    Create DorkFb.py

---
+++ x/Tools/fb-prank/DorkFb.py
@@ -0,0 +1,10 @@
+import error
+from error.id import *
+from error.it import *
+from error.jp import *
+from error.us import *
+from error.fr import *
+from error.kr import *
+from error.de import *
+from error.tr import *
+import error,re,os
