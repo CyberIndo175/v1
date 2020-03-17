56936e1f764a610bd8d1222576667ae20b288e7c
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 23:50:04 +0700

    Create logo.sh

---
+++ x/Tools/tombol_special_termux/logo.sh
@@ -0,0 +1,24 @@
+#usr/bin!/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+hi='\033[0;30m' #Hitam
+echo $ku"	╔═══════════════════════════════════════╗"
+echo $ku"	║"$bi"╔╦╗╔═╗╔╦╗╔╗ ╔═╗╦    ╔═╗╔═╗╔═╗╔═╗╦╔═╗╦"$ku"  ║"
+echo $ku"	║"$bi" ║ ║ ║║║║╠╩╗║ ║║    ╚═╗╠═╝║╣ ║  ║╠═╣║"$ku"  ║"
+echo $ku"	║"$bi" ╩ ╚═╝╩ ╩╚═╝╚═╝╩═╝  ╚═╝╩  ╚═╝╚═╝╩╩ ╩╩═╝"$ku"║"
+echo $ku"	╚═══════════════════════════════════════╝"
+echo $me"          	╔═══════════════════╗"
+echo $bi"          	║"$ku"╔╦╗╔═╗╦═╗╔╦╗╦ ╦═╗ ╦"$me"║"
+echo $bi"          	║"$ku" ║ ║╣ ╠╦╝║║║║ ║╔╩╦╝"$me"║"
+echo $bi"          	║"$ku" ╩ ╚═╝╩╚═╩ ╩╚═╝╩ ╚═"$me"║"
+echo $bi"          	╚═══════════════════╝"
+echo $pu"  	["$ku"ESC"$pu"] ["$ku"/"$pu"] ["$ku"-"$pu"] ["$ku"HOME"$pu"] ["$ku"/"$pu"] ["$ku"END"$pu"] ["$ku"PGUP"$pu"]"
+echo $me"       <═════════════════════════════════════>"
+echo $pu" Author"$me":"$ku"Mr.!nSt4rneXt1"$me"|"$pu"Team"$me":"$ku"Cyber Hacking Indonesia"
+echo
