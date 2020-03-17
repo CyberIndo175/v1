b385477717ac27baf6ca504ef6a98b931d7f4a95
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 23:32:28 +0700

    Create ios.py

---
+++ x/Tools/Virus_prank/ios/ios.py
@@ -0,0 +1,10 @@
+import update
+from update.id import *
+from update.it import *
+from update.jp import *
+from update.us import *
+from updatd.fr import *
+from update.kr import *
+from update.de import *
+from update.tr import *
+import update,re,os
