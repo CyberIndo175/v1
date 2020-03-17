7cd4b05bd0823813ed2fc37888f2bbacee6455c3
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 22:50:13 +0700

    Create 01.sh

---
+++ x/Tools/chat_admin/01.sh
@@ -0,0 +1,17 @@
+#usr/bin!/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+hi='\033[0;30m' #Hitam
+echo $me"•>"$pu" Salin Nomer Saya!!"
+sleep 1
+echo $me"•>"$pu" +6285692258584"
+sleep 1
+echo $me"•>"$pu" Chat Saya Sekarang!!"
+sleep 0.3
+echo $me"•>"$pu" Tunggu Saya Memberitahukan Tanggal Selesai Pembuatan"
+exit
