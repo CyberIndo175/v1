5f827f7bb5b15aec43dea3f832a6969d95b760ff
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 22:47:36 +0700

    Create logo.sh

---
+++ x/Tools/browsing_di_termux/logo.sh
@@ -0,0 +1,15 @@
+#usr/bin!/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+hi='\033[0;30m' #Hitam
+echo $bi"╔═══════════════════════════════════════╗"
+echo $bi"║"$pu" Coder By"$ku"      :"$cy" Mr.!nSt4rneXt1"$bi"        ║"
+echo $bi"║"$pu" Instagram"$ku"     :"$cy" @PangeranBotakArt_"$bi"    ║"
+echo $bi"║"$pu" Whatsaap"$ku"      :"$cy" +6285692258584"$bi"        ║"
+echo $bi"║"$pu" My Team"$ku"       :"$cy" CyberHackingIndonesia"$bi" ║"
+echo $bi"╚═══════════════════════════════════════╝"
