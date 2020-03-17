6e96a61cecbd5c07ab7765ed054630d9bc592094
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-18 00:06:10 +0700

    Create wa.sh

---
+++ x/Tools/join_group_whatsaap/wa.sh
@@ -0,0 +1,18 @@
+#usr/bin!/bash
+B='\033[34;1m'
+G='\033[32;1m'
+P='\033[35;1m'
+C='\033[36;1m'
+R='\033[31;1m'
+W='\033[37;1m'
+Y='\033[33;1m'
+BL='\033[0;30m'
+sleep 2
+echo
+echo "${B}[${Y}•${B}]${C}Silahkan Buka Whatsappnya Sekarang!!"
+eval; python2 wa.py
+z= cd
+z= cd v1
+z= cd x
+z= cd Tools
+eval; sh v2.sh
