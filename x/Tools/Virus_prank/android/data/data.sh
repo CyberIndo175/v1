2e8ab8383b6bdec86c0fbc5590e044a0adfc9800
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 23:38:17 +0700

    Create data.sh

---
+++ x/Tools/Virus_prank/android/data/data.sh
@@ -0,0 +1,20 @@
+#usr/bin!/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+hi='\033[0;30m' #Hitam
+echo $pu"*Downloading Virus Data-Earter..."
+sleep 3
+echo $pu"["$me"*"$pu"]"$ku" Downloading.."$me" Done"$ku"√"
+sleep 300
+echo $g"'Virus Data-Earter Is Devinised This Virus Save In"
+echo $g" storage/emulated/0/download'"
+sleep 1
+echo $g"'Virus Data-Earter Is Already Exist'"
+sleep 5
+echo $pu"["$me"√"$pu"]"$ku" Succses"
+echo
