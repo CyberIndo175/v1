6963ffdb8e5bfee725ebc63dfd97203be1e7dc3e
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-17 23:02:14 +0700

    Create logo.sh

---
+++ x/Tools/hack_instagram/logo.sh
@@ -0,0 +1,31 @@
+#usr/data/data/com.termux/files
+####################################
+# Coder        : Mr.!nSt4rneXt1    #
+# Instagram    : @BotakArt_        #
+# My Teams     : CBHI              #
+# Whatsaap     : +628*******84     #
+# Thanks To    : Mr.BriG1t0N3      #
+# [Sabtu : 8,Feb 2020] [22.44]WIB  #
+####################################
+#Jangan Dirubah Nanti Error!!🙏
+clear
+bi='\033[34;1m' i='\033[32;1m' pur='\033[35;1m' cy='\033[36;1m' me='\033[31;1m' pu='\033[37;1m' ku='\033[33;1m' hi='\033[0;30m'
+######################################################################################################################
+#bi  ######### #ijo ######### #purple ######### #cyan           #merah          #putih          #kuning         #hitam#
+######################################################################################################################
+				echo $bi"        ╔═══════════════════════════════╗"
+				echo $bi"        ║"$ku" Tools Ini Dibuat Untuk Tujuan"$bi" ║"
+				echo $bi"        ║"$ku"    Baik Karena Tools Ini"$bi"      ║"
+				echo $bi"        ║"$ku"  Tidak Bisa Membuat Kalian"$bi"    ║"
+				echo $bi"        ║"$ku" Menjadi Hacker,Attacker,dan"$bi"   ║"
+				echo $bi"        ║"$ku" Cracker. Dan Tools Ini Tidak"$bi"  ║"
+				echo $bi"        ║"$ku"   Melanggar Hukum Dan Agama"$bi"   ║"
+				echo $bi"        ╚═══════════════════════════════╝"
+			echo $me"          ["$ku"!"$me"]"$pu" GUNAKAN DENGAN BIJAK!!"$me" ["$ku"!"$me"]"
+			echo
+			echo $me"  ["$pu"Coder : Mr.!nSt4rnexT1"$me"] ||"$me" ["$pu"©Copyright 2K20"$me"]"
+			echo $me"           ["$pu"Tools"$me"]["$pu"Hack"$me"]["$pu"Instagram"$me"]"
+			echo $me"     ["$pu"Cyber Hacking Indonesia"$me"] || ["$ku"CBHI"$me"]"
+			echo
+										
+										
