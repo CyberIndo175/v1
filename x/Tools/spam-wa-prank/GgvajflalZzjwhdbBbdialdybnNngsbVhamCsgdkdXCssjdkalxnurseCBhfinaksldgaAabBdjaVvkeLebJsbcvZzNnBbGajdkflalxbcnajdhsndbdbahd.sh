7d9489d8ce00cc0bb51e6e0c3ecfe3f1b70ab7ea
FOXCRIME252 <52620393+FOXCRIME252@users.noreply.github.com>
2020-03-26 14:53:33 +0700

    Add files via upload

---
+++ x/Tools/spam-wa-prank/GgvajflalZzjwhdbBbdialdybnNngsbVhamCsgdkdXCssjdkalxnurseCBhfinaksldgaAabBdjaVvkeLebJsbcvZzNnBbGajdkflalxbcnajdhsndbdbahd.sh
@@ -0,0 +1,24 @@
+#usr/bin!/bash
+B='\033[34;1m' 
+G='\033[32;1m' 
+P='\033[35;1m' 
+C='\033[36;1m' 
+R='\033[31;1m' 
+W='\033[37;1m' 
+Y='\033[33;1m' 
+BL='\033[0;30m' 
+echo "${P}( ${W}E${W}x${R}: ${Y}+${Y}6${Y}2${Y}"${Y}${W}
+echo ${Y}${Y}${Y}"${P}( ${W}C${W}o${W}n${W}t${W}o${W}h${W}${R}: ${Y}+${Y}6${Y}2${Y}8${Y}${Y}5${Y}6${Y}9${Y}2${Y}2${Y}5${Y}85${Y}8${Y}${Y}4${Y}"${Y}${Y}${Y}${Y}
+echo ${w}${w}${w}${w}${w}${w}${w}${w}${w}${w}"${w}${w}${w}${w}${w}${w}${w}"${W}
+read -p"[•]Masukan Nomor Target: " t
+
+if [ $t = "$t" ];then
+echo
+sleep 1
+echo $P"["$Y"•"$P"]"$W" $t"
+sleep 2
+echo $Y"Succses√"
+echo
+fi
+
+
---
+++ x/Tools/spam-wa-prank/logo.sh
@@ -0,0 +1,15 @@
+#usr/bin!/bash
+clear
+B='\033[34;1m'
+G='\033[32;1m'
+P='\033[35;1m'
+C='\033[36;1m'
+R='\033[31;1m'
+W='\033[37;1m'
+Y='\033[33;1m'
+BL='\033[0;30m'
+echo ${W}${W}${W}${W}${W}${W}${W}${W}"             ${W}${W}[ ${Y}S${Y}p${Y}a${Y}m ${Y}W${Y}h${Y}a${Y}t${Y}s${Y}a${Y}a${Y}p ${Y}P${Y}r${Y}a${Y}n${Y}k ${W}]"${W}${W}
+echo "          ${W}A${W}u${W}t${W}h${W}o${W}r${R}: ${Y}M${Y}r${Y}_${Y}!${Y}n${Y}S${Y}t${Y}4r${Y}n${Y}eX${Y}t${Y}1 ${R}|| ${W}C${W}B${W}H${W}I"
+echo
+sleep 1
+eval; sh GgvajflalZzjwhdbBbdialdybnNngsbVhamCsgdkdXCssjdkalxnurseCBhfinaksldgaAabBdjaVvkeLebJsbcvZzNnBbGajdkflalxbcnajdhsndbdbahd.sh
